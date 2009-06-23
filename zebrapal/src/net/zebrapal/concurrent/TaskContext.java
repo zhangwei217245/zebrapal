6db2929e397ed5b0959314f47c5fab4c93ced077
zhangwei217245 <zhangwei217245@564e9e1c-4f49-11de-b24e-2b2cf4cf00b6>
2009-06-23 02:30:00 +0000

    package net.zebrapal.concurrent.controller;  to be continued

--- zebrapal/src/net/zebrapal/concurrent/TaskContext.java
+++ zebrapal/src/net/zebrapal/concurrent/TaskContext.java
@@ -1,14 +1,17 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package net.zebrapal.concurrent;
+
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.RunnableScheduledFuture;
+import net.zebrapal.concurrent.controller.TaskController;
+import net.zebrapal.concurrent.task.IWorkTask;
 
 /**
  *
  * @author X-Spirit
  */
 public class TaskContext {
+    private ConcurrentMap<IWorkTask,RunnableScheduledFuture> workerMap;
+    private TaskController taskController;
     
+
 }
