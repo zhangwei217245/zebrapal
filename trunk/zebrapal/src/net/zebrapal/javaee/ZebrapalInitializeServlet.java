10a30a3669da61b71b2b3351d231f5e53c284333
zhangwei217245 <zhangwei217245@564e9e1c-4f49-11de-b24e-2b2cf4cf00b6>
2009-06-25 11:09:33 +0000

    package net.zebrapal.concurrent.controller;  to be continued

--- trunk/zebrapal/src/net/zebrapal/javaee/ZebrapalInitializeServlet.java
+++ trunk/zebrapal/src/net/zebrapal/javaee/ZebrapalInitializeServlet.java
@@ -7,10 +7,13 @@ package net.zebrapal.javaee;
 
 import java.io.IOException;
 import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import net.zebrapal.concurrent.TaskContext;
+import net.zebrapal.concurrent.ZebrapalContextLoader;
 
 /**
  *
@@ -19,6 +22,10 @@ import javax.servlet.http.HttpServletResponse;
 public class ZebrapalInitializeServlet extends HttpServlet{
     private static final long serialVersionUID = -4135075203934826888L;
     public static final String ZEBRAPAL_CONTEXT_KEY="ZEBRAPAL_CONTEXT_KEY";
+    public static final String ZEBRAPAL_CONTEXT_LOADER="ZEBRAPAL_CONTEXT_LOADER";
+    public static final String ZEBRAPAL_CONTEXT_CONFIG_FILE="ZEBRAPAL_CONTEXT_CONFIG_FILE";
+
+    private TaskContext taskContext;
     /**
      * Initialize this Servlet and create ZebrapalContext
      * @param cfg
@@ -27,13 +34,49 @@ public class ZebrapalInitializeServlet extends HttpServlet{
     @Override
     public void init(ServletConfig cfg) throws javax.servlet.ServletException{
         super.init(cfg);
-        log("Zebrapal Context is initializing...");
         
+        ServletContext sc = cfg.getServletContext();
+        String configFileName = cfg.getInitParameter(ZEBRAPAL_CONTEXT_CONFIG_FILE);
+        String contextLoaderClass = cfg.getInitParameter(ZEBRAPAL_CONTEXT_LOADER);
+        try {
+            log("Zebrapal Context is initializing...");
+            if(contextLoaderClass!=null){
+                Object obj = Class.forName(contextLoaderClass).newInstance();
+                if(obj instanceof ZebrapalContextLoader){
+                    taskContext=((ZebrapalContextLoader)obj).loadContext(configFileName);
+                }else{
+                    throw new Exception("You spcified a wrong Context Loader.");
+                }
+                if(taskContext==null){
+                    throw new Exception("TaskContext cannot be loaded due to previous error.");
+                }else{
+                    if(sc.getAttribute(ZEBRAPAL_CONTEXT_KEY)!=null){
+                        throw new Exception("zebrapal task context is already loaded in the ServletContext.");
+                    }
+                    sc.setAttribute(ZEBRAPAL_CONTEXT_KEY, taskContext);
+                }
+            }else{
+                throw new Exception("ContextLoader is not specified.");
+            }
+        } catch (Exception e) {
+            log(e.getMessage(), e);
+        }
+        log("Zebrapal Context is successfully initialized...");
     }
 
     @Override
     public void destroy() {
-        super.destroy();
+        try {
+            log("Destroying Zebrapal Context ...");
+            if(taskContext==null){
+                throw new Exception("TaskContext has been unloaded.");
+            }else{
+                taskContext.destroy();
+            }
+        }catch(Exception e){
+            log(e.getMessage(), e);
+        }
+        log("Zebrapal Context is successfully destroyed...");
     }
 
     @Override
